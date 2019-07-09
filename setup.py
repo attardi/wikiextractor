daaee647eb227892b6c8b993ee2a880d563222ea
mouhamadaboshokor <mouhamadaboshokor@gmail.com>
2019-07-09 13:55:18 +0300

    pypi files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='WIKIextractor',
+    packages=['WIKIextractor'],
+    version='0.1',
+    license='MIT',
+    description='scripts for parsing the wikimedia xml dumps files',
+    author='',
+    author_email='',
+    url='https://github.com/MouhamadAboShokor/wikiextractor',
+    download_url='https://github.com/MouhamadAboShokor/wikiextractor/v_01.tar.gz',
+    keywords=[''],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: DataScience :: WikiMedia',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
